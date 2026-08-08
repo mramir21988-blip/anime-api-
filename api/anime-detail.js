fcd18f6469d30ce8d31735ceaeac10d92b2b75e3
mramir21988-blip <mramir21988@gmail.com>
2026-08-08 06:00:05 +0530

    Update anime-detail.js

--- api/anime-detail.js
+++ api/anime-detail.js
@@ -1,1 +1,9 @@
-
+const allData = require('../data/anime-data.json');
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const { id } = req.query;
+  if (!id) return res.status(400).json({ success: false, message: 'ID required' });
+  const anime = allData.find(a => a.id === id);
+  if (!anime) return res.status(404).json({ success: false, message: 'Anime not found' });
+  res.json({ success: true, data: anime });
+};
